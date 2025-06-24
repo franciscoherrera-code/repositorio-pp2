b1c737a9b2037900f5f0b80040cd661728234c16
Francisco <tachi290998.fh@gmail.com>
2025-06-24 10:55:32 -0300

    Update db.php
    detalle las columnas de la tabla de la base de datos y agrego comentarios

--- maps/db.php
+++ maps/db.php
@@ -1,11 +1,21 @@
 <?php
+/*previamente cree la base de datos llamada logistica. en sql:
+CREATE TABLE paquetes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    direccion TEXT NOT NULL,
+    latitud FLOAT,
+    longitud FLOAT
+); */
+
+// Datos de conexión al servidor MySQL
 $host = "localhost";
 $user = "root";
 $pass = "";
-$dbname = "ejemplo";
-
+$dbname = "logistica"; //nombre de la base de datos
+// Crea una nueva conexión con MySQL usando mysqli
 $conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
+    // Si no hay errores, la conexión queda abierta y lista para usar
 ?>
